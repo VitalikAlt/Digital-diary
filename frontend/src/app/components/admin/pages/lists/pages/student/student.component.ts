430b093119f85a2ce274d5f26df164336a1c2f4b
vitaly <vitaliksomeman@gmail.com>
2018-08-08 18:30:53 +0300

    fix checkboxs, fix toast on login already exist

--- frontend/src/app/components/admin/pages/lists/pages/student/student.component.ts
+++ frontend/src/app/components/admin/pages/lists/pages/student/student.component.ts
@@ -1,109 +1,149 @@
-import { Component, OnInit, EventEmitter, ViewChild } from '@angular/core';
+import { Component, OnInit, EventEmitter, ViewChild } from "@angular/core";
 import { MaterializeAction, toast } from "angular2-materialize";
-import { HttpService } from '../../../../../../services/http.service';
-import { FadeInOutAnimation } from '../../../../../../animations/FadeInOutAnimation';
-import { Md5 } from 'ts-md5/dist/md5';
+import { HttpService } from "../../../../../../services/http.service";
+import { FadeInOutAnimation } from "../../../../../../animations/FadeInOutAnimation";
+import { Md5 } from "ts-md5/dist/md5";
 
 @Component({
-  selector: 'app-admin-lists',
-  templateUrl: 'student.component.html',
-  styleUrls: ['student.component.css'],
+  selector: "app-admin-lists",
+  templateUrl: "student.component.html",
+  styleUrls: ["student.component.css"],
   animations: [FadeInOutAnimation]
 })
 export class StudentListComponent implements OnInit {
+  @ViewChild("updateStudentModal") updateStudentModal;
 
-  @ViewChild('updateStudentModal') updateStudentModal;
-
-  public addStudentModal = new EventEmitter<string|MaterializeAction>();
-  public changePasswordModal = new EventEmitter<string|MaterializeAction>();
-  public changeStudentModal = new EventEmitter<string|MaterializeAction>();
-  public deleteStudentModal = new EventEmitter<string|MaterializeAction>();
+  public addStudentModal = new EventEmitter<string | MaterializeAction>();
+  public changePasswordModal = new EventEmitter<string | MaterializeAction>();
+  public changeStudentModal = new EventEmitter<string | MaterializeAction>();
+  public deleteStudentModal = new EventEmitter<string | MaterializeAction>();
 
   public modalUser;
 
-  public studentSorts: [string, boolean] = ['name', false];
-  public studentFilters: {name, course, squad} = {name: '', course: '', squad: ''};
+  public studentSorts: [string, boolean] = ["name", false];
+  public studentFilters: { name; course; squad } = {
+    name: "",
+    course: "",
+    squad: ""
+  };
   public studentsForDelete: Array<Object> = [];
 
   public students: Array<Object> = [];
 
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {}
 
   ngOnInit() {
     this.modalUser = {};
 
-    this.httpService.getStudentList()
-      .subscribe((students) => {
+    this.httpService.getStudentList().subscribe(
+      students => {
         this.students = students;
-        console.log(this.students)
-      }, (error) => {
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
+        console.log(this.students);
+      },
+      error => {
+        toast("Неизвестная ошибка!", 4000, "error-toast");
         console.log(error);
-      })
+      }
+    );
   }
 
   addStudent() {
-    if (!this.modalUser.course || !this.modalUser.squad || !this.modalUser.surname || !this.modalUser.name
-      || !this.modalUser.fatherName || !this.modalUser.login || !this.modalUser.password)
-      return toast('Заполнены не все поля!', 4000, 'error-toast');
+    if (
+      !this.modalUser.course ||
+      !this.modalUser.squad ||
+      !this.modalUser.surname ||
+      !this.modalUser.name ||
+      !this.modalUser.fatherName ||
+      !this.modalUser.login ||
+      !this.modalUser.password
+    )
+      return toast("Заполнены не все поля!", 4000, "error-toast");
 
     if (!Number(this.modalUser.course) || !Number(this.modalUser.squad))
-      return toast('Неверный курс или группа!', 4000, 'error-toast');
+      return toast("Неверный курс или группа!", 4000, "error-toast");
 
-    this.httpService.addStudent(
-      Number(this.modalUser.course), Number(this.modalUser.squad), this.modalUser.surname,
-      this.modalUser.name, this.modalUser.fatherName, this.modalUser.login,
-      Md5.hashStr(`_${this.modalUser.password}_`).toString()
-    )
-      .subscribe((id) => {
-        this.students.push({
-          id: id,
-          name: `${this.modalUser.surname} ${this.modalUser.name[0]}. ${this.modalUser.fatherName[0]}.`,
-          course: this.modalUser.course,
-          squad: this.modalUser.squad
-        });
+    this.httpService
+      .addStudent(
+        Number(this.modalUser.course),
+        Number(this.modalUser.squad),
+        this.modalUser.surname,
+        this.modalUser.name,
+        this.modalUser.fatherName,
+        this.modalUser.login,
+        Md5.hashStr(`_${this.modalUser.password}_`).toString()
+      )
+      .subscribe(
+        id => {
+          this.students.push({
+            id: id,
+            name: `${this.modalUser.surname} ${this.modalUser.name[0]}. ${
+              this.modalUser.fatherName[0]
+            }.`,
+            course: this.modalUser.course,
+            squad: this.modalUser.squad
+          });
 
-        toast('Студент добавлен!', 4000, 'success-toast');
-        this.closeAddStudentModal();
-      }, (error) => {
-        if (error === 'User already exist!')
-          return toast('Студент уже существует!', 4000, 'error-toast');
+          toast("Студент добавлен!", 4000, "success-toast");
+          this.closeAddStudentModal();
+        },
+        error => {
+          if (error === "User already exist!")
+            return toast(
+              "Данный логин занят, пожалуйста используйте другой!",
+              4000,
+              "error-toast"
+            );
 
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
-        console.log(error);
-      })
+          toast("Неизвестная ошибка!", 4000, "error-toast");
+          console.log(error);
+        }
+      );
   }
 
   changePassword() {
-    this.httpService.changePassword(this.modalUser.login, Md5.hashStr(`_${this.modalUser.new_password}_`).toString())
-      .subscribe((result) => {
-        toast('Студент успешно изменён!', 4000, 'success-toast');
-        this.closeChangePasswordModal();
-      }, (error) => {
-        if (error === "No user with that id!")
-          return toast('Студент не найден, обновите страницу!', 4000, 'error-toast');
+    this.httpService
+      .changePassword(
+        this.modalUser.login,
+        Md5.hashStr(`_${this.modalUser.new_password}_`).toString()
+      )
+      .subscribe(
+        result => {
+          toast("Студент успешно изменён!", 4000, "success-toast");
+          this.closeChangePasswordModal();
+        },
+        error => {
+          if (error === "No user with that id!")
+            return toast(
+              "Студент не найден, обновите страницу!",
+              4000,
+              "error-toast"
+            );
 
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
-        console.log(error);
-      })
+          toast("Неизвестная ошибка!", 4000, "error-toast");
+          console.log(error);
+        }
+      );
   }
 
   deleteStudents() {
     const ids = [];
 
     for (let i = 0; i < this.studentsForDelete.length; i++) {
-      ids.push(this.studentsForDelete[i]['id']);
+      ids.push(this.studentsForDelete[i]["id"]);
     }
 
-    this.httpService.deleteStudents(ids).subscribe(() => {
+    this.httpService.deleteStudents(ids).subscribe(
+      () => {
         this.studentsForDelete = [];
         this.ngOnInit();
-        toast('Список студентов обновлён!', 4000, 'success-toast');
+        toast("Список студентов обновлён!", 4000, "success-toast");
         this.closeDeleteStudentModal();
-      }, (error) => {
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
+      },
+      error => {
+        toast("Неизвестная ошибка!", 4000, "error-toast");
         console.log(error);
-      })
+      }
+    );
   }
 
   setDeletedStudent(student) {
@@ -114,10 +154,8 @@ export class StudentListComponent implements OnInit {
   changeDeletedStudents(student) {
     const index = this.studentsForDelete.indexOf(student);
 
-    if (index === -1)
-      this.studentsForDelete.push(student);
-    else
-      this.studentsForDelete.splice(index, 1);
+    if (index === -1) this.studentsForDelete.push(student);
+    else this.studentsForDelete.splice(index, 1);
   }
 
   changeStudentSort(type: string) {
@@ -125,41 +163,47 @@ export class StudentListComponent implements OnInit {
   }
 
   changeSort(sort: Object, newType: string) {
-    sort[1] = (sort[0] === newType)? !sort[1] : false;
+    sort[1] = sort[0] === newType ? !sort[1] : false;
     sort[0] = newType;
   }
 
   openAddStudentModal() {
-    this.addStudentModal.emit({action:"modal",params:['open']});
+    this.addStudentModal.emit({ action: "modal", params: ["open"] });
   }
 
   closeAddStudentModal() {
-    this.addStudentModal.emit({action:"modal",params:['close']});
+    this.addStudentModal.emit({ action: "modal", params: ["close"] });
   }
 
   openChangePasswordModal() {
-    this.httpService.getStudentUserData(this.modalUser.id)
-      .subscribe((result) => {
-        this.changePasswordModal.emit({action:"modal",params:['open']});
+    this.httpService.getStudentUserData(this.modalUser.id).subscribe(
+      result => {
+        this.changePasswordModal.emit({ action: "modal", params: ["open"] });
         this.modalUser = result;
-      }, (error) => {
+      },
+      error => {
         if (error === "No user with that id!")
-          return toast('Студент не найден, обновите страницу!', 4000, 'error-toast');
+          return toast(
+            "Студент не найден, обновите страницу!",
+            4000,
+            "error-toast"
+          );
 
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
+        toast("Неизвестная ошибка!", 4000, "error-toast");
         console.log(error);
-      });
+      }
+    );
   }
 
   closeChangePasswordModal() {
-    this.changePasswordModal.emit({action:"modal",params:['close']});
+    this.changePasswordModal.emit({ action: "modal", params: ["close"] });
   }
 
   openDeleteStudentModal() {
-    this.deleteStudentModal.emit({action:"modal",params:['open']});
+    this.deleteStudentModal.emit({ action: "modal", params: ["open"] });
   }
 
   closeDeleteStudentModal() {
-    this.deleteStudentModal.emit({action:"modal",params:['close']});
+    this.deleteStudentModal.emit({ action: "modal", params: ["close"] });
   }
 }
--- frontend/src/app/components/admin/pages/lists/pages/teacher/teacher.component.ts
+++ frontend/src/app/components/admin/pages/lists/pages/teacher/teacher.component.ts
@@ -1,102 +1,134 @@
-import { Component, OnInit, EventEmitter, ViewChild } from '@angular/core';
+import { Component, OnInit, EventEmitter, ViewChild } from "@angular/core";
 import { MaterializeAction, toast } from "angular2-materialize";
-import { HttpService } from '../../../../../../services/http.service';
-import { Md5 } from 'ts-md5/dist/md5';
-import { FadeInOutAnimation } from '../../../../../../animations/FadeInOutAnimation';
+import { HttpService } from "../../../../../../services/http.service";
+import { Md5 } from "ts-md5/dist/md5";
+import { FadeInOutAnimation } from "../../../../../../animations/FadeInOutAnimation";
 
 @Component({
-  selector: 'app-admin-lists',
-  templateUrl: 'teacher.component.html',
-  styleUrls: ['teacher.component.css'],
+  selector: "app-admin-lists",
+  templateUrl: "teacher.component.html",
+  styleUrls: ["teacher.component.css"],
   animations: [FadeInOutAnimation]
 })
 export class TeacherListComponent implements OnInit {
+  @ViewChild("updateTeacherModal") updateTeacherModal;
 
-  @ViewChild('updateTeacherModal') updateTeacherModal;
-
-  public addTeacherModal = new EventEmitter<string|MaterializeAction>();
-  public changePasswordModal = new EventEmitter<string|MaterializeAction>();
-  public changeTeacherModal = new EventEmitter<string|MaterializeAction>();
-  public deleteTeacherModal = new EventEmitter<string|MaterializeAction>();
+  public addTeacherModal = new EventEmitter<string | MaterializeAction>();
+  public changePasswordModal = new EventEmitter<string | MaterializeAction>();
+  public changeTeacherModal = new EventEmitter<string | MaterializeAction>();
+  public deleteTeacherModal = new EventEmitter<string | MaterializeAction>();
 
   public modalUser;
 
-  public teacherSorts: [string, boolean] = ['name', false];
-  public teacherFilters: {name} = {name: ''};
+  public teacherSorts: [string, boolean] = ["name", false];
+  public teacherFilters: { name } = { name: "" };
   public teachersForDelete: Array<Object> = [];
 
   public teachers: Array<Object> = [];
 
-  constructor(private httpService: HttpService) { }
+  constructor(private httpService: HttpService) {}
 
   ngOnInit() {
     this.modalUser = {};
 
-    this.httpService.getTeacherList()
-      .subscribe((teachers) => {
+    this.httpService.getTeacherList().subscribe(
+      teachers => {
         this.teachers = teachers;
-      }, (error) => {
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
+      },
+      error => {
+        toast("Неизвестная ошибка!", 4000, "error-toast");
         console.log(error);
-      })
+      }
+    );
   }
 
   addTeacher() {
-    if (!this.modalUser.surname || !this.modalUser.name
-      || !this.modalUser.fatherName || !this.modalUser.login || !this.modalUser.password)
-      return toast('Заполнены не все поля!', 4000, 'error-toast');
-
-    this.httpService.addTeacher(
-      this.modalUser.surname, this.modalUser.name, this.modalUser.fatherName,
-      this.modalUser.login, Md5.hashStr(`_${this.modalUser.password}_`).toString()
+    if (
+      !this.modalUser.surname ||
+      !this.modalUser.name ||
+      !this.modalUser.fatherName ||
+      !this.modalUser.login ||
+      !this.modalUser.password
     )
-      .subscribe((id) => {
-        this.teachers.push({
-          id: id,
-          name: `${this.modalUser.surname} ${this.modalUser.name[0]}. ${this.modalUser.fatherName[0]}.`
-        });
+      return toast("Заполнены не все поля!", 4000, "error-toast");
 
-        toast('Преподаватель добавлен!', 4000, 'success-toast');
-        this.closeAddTeacherModal();
-      }, (error) => {
-        if (error === 'User already exist!')
-          return toast('Преподаватель уже существует!', 4000, 'error-toast');
+    this.httpService
+      .addTeacher(
+        this.modalUser.surname,
+        this.modalUser.name,
+        this.modalUser.fatherName,
+        this.modalUser.login,
+        Md5.hashStr(`_${this.modalUser.password}_`).toString()
+      )
+      .subscribe(
+        id => {
+          this.teachers.push({
+            id: id,
+            name: `${this.modalUser.surname} ${this.modalUser.name[0]}. ${
+              this.modalUser.fatherName[0]
+            }.`
+          });
 
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
-        console.log(error);
-      })
+          toast("Преподаватель добавлен!", 4000, "success-toast");
+          this.closeAddTeacherModal();
+        },
+        error => {
+          if (error === "User already exist!")
+            return toast(
+              "Данный логин занят, пожалуйста используйте другой!",
+              4000,
+              "error-toast"
+            );
+
+          toast("Неизвестная ошибка!", 4000, "error-toast");
+          console.log(error);
+        }
+      );
   }
 
   changePassword() {
-    this.httpService.changePassword(this.modalUser.login, Md5.hashStr(`_${this.modalUser.new_password}_`).toString())
-      .subscribe((result) => {
-        toast('Пароль успешно изменён!', 4000, 'success-toast');
-        this.closeChangePasswordModal();
-      }, (error) => {
-        if (error === "No user with that id!")
-          return toast('Преподаватель не найден, обновите страницу!', 4000, 'error-toast');
+    this.httpService
+      .changePassword(
+        this.modalUser.login,
+        Md5.hashStr(`_${this.modalUser.new_password}_`).toString()
+      )
+      .subscribe(
+        result => {
+          toast("Пароль успешно изменён!", 4000, "success-toast");
+          this.closeChangePasswordModal();
+        },
+        error => {
+          if (error === "No user with that id!")
+            return toast(
+              "Преподаватель не найден, обновите страницу!",
+              4000,
+              "error-toast"
+            );
 
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
-        console.log(error);
-      })
+          toast("Неизвестная ошибка!", 4000, "error-toast");
+          console.log(error);
+        }
+      );
   }
 
   deleteTeachers() {
     const ids = [];
 
     for (let i = 0; i < this.teachersForDelete.length; i++) {
-      ids.push(this.teachersForDelete[i]['id']);
+      ids.push(this.teachersForDelete[i]["id"]);
     }
 
-    this.httpService.deleteTeachers(ids)
-      .subscribe(() => {
+    this.httpService.deleteTeachers(ids).subscribe(
+      () => {
         this.ngOnInit();
         this.teachersForDelete = [];
         this.closeDeleteTeacherModal();
-      }, (error) => {
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
+      },
+      error => {
+        toast("Неизвестная ошибка!", 4000, "error-toast");
         console.log(error);
-      })
+      }
+    );
   }
 
   setDeletedTeacher(teacher) {
@@ -107,10 +139,8 @@ export class TeacherListComponent implements OnInit {
   changeDeletedTeachers(teacher) {
     const index = this.teachersForDelete.indexOf(teacher);
 
-    if (index === -1)
-      this.teachersForDelete.push(teacher);
-    else
-      this.teachersForDelete.splice(index, 1);
+    if (index === -1) this.teachersForDelete.push(teacher);
+    else this.teachersForDelete.splice(index, 1);
   }
 
   changeTeacherSort(type: string) {
@@ -118,41 +148,47 @@ export class TeacherListComponent implements OnInit {
   }
 
   changeSort(sort: Object, newType: string) {
-    sort[1] = (sort[0] === newType)? !sort[1] : false;
+    sort[1] = sort[0] === newType ? !sort[1] : false;
     sort[0] = newType;
   }
 
   openAddTeacherModal() {
-    this.addTeacherModal.emit({action:"modal",params:['open']});
+    this.addTeacherModal.emit({ action: "modal", params: ["open"] });
   }
 
   closeAddTeacherModal() {
-    this.addTeacherModal.emit({action:"modal",params:['close']});
+    this.addTeacherModal.emit({ action: "modal", params: ["close"] });
   }
 
   openChangePasswordModal() {
-    this.httpService.getTeacherUserData(this.modalUser.id)
-      .subscribe((result) => {
-        this.changePasswordModal.emit({action:"modal",params:['open']});
+    this.httpService.getTeacherUserData(this.modalUser.id).subscribe(
+      result => {
+        this.changePasswordModal.emit({ action: "modal", params: ["open"] });
         this.modalUser = result;
-      }, (error) => {
+      },
+      error => {
         if (error === "No user with that id!")
-          return toast('Преподаватель не найден, обновите страницу!', 4000, 'error-toast');
+          return toast(
+            "Преподаватель не найден, обновите страницу!",
+            4000,
+            "error-toast"
+          );
 
-        toast('Неизвестная ошибка!', 4000, 'error-toast');
+        toast("Неизвестная ошибка!", 4000, "error-toast");
         console.log(error);
-      });
+      }
+    );
   }
 
   closeChangePasswordModal() {
-    this.changePasswordModal.emit({action:"modal",params:['close']});
+    this.changePasswordModal.emit({ action: "modal", params: ["close"] });
   }
 
   openDeleteTeacherModal() {
-    this.deleteTeacherModal.emit({action:"modal",params:['open']});
+    this.deleteTeacherModal.emit({ action: "modal", params: ["open"] });
   }
 
   closeDeleteTeacherModal() {
-    this.deleteTeacherModal.emit({action:"modal",params:['close']});
+    this.deleteTeacherModal.emit({ action: "modal", params: ["close"] });
   }
 }
